911386208ad72695d4e7186ce956da2b6cca0ff0
Michael Matiu <michaelmatiu@gmail.com>
2023-01-24 14:26:27 +0100

    remo crop select by indices

--- py/download-full-gar-06b-remo-crop.py
+++ py/download-full-gar-06b-remo-crop.py
@@ -1,9 +1,9 @@
-# crop orog from wget
+# crop variable by variable from wget
 
 # delete manually the non-cropped files afterwards!!
 
-path_in = "/home/climatedata/eurocordex-temp/orog/"
-path_out = "/home/climatedata/eurocordex-temp2/orog/"
+path_in = "/home/climatedata/eurocordex-temp/tasmax/"
+path_out = "/home/climatedata/eurocordex-temp3/tasmax/"
 
 import os
 from glob import glob
@@ -11,20 +11,20 @@ import xarray as xr
 from datetime import datetime # for info
 
 all_files = glob(os.path.join(path_in, "*.nc"))
+all_files.sort()
+
+os.makedirs(path_out, exist_ok=True)
   
 for file_in in all_files:
   file_out = os.path.join(path_out, os.path.basename(file_in))
+  
   if(not os.path.exists(file_out)):
+    
     ds = xr.open_dataset(file_in, chunks={'time': 120})
     
     ds_info = os.path.basename(file_in).split("_")
-    ds_rcm = ds_info[5]
-    if ds_rcm != "MPI-CSC-REMO2009":
-      ds = ds.sel(rlat=slice(-8, -1), rlon=slice(-11, 0))
-    else:
-      # same indices, same as cdo setgrid
-      ds = ds.sel(rlat=slice(-8 + 0.055, -1 + 0.055), rlon=slice(-11 - 0.055, 0 - 0.055))
-
+    # ds = ds.sel(rlat=slice(-8, -1), rlon=slice(-11, 0))
+    ds = ds.sel(rlat=slice(-8 + 0.055, -1 + 0.055), rlon=slice(-11 - 0.055, 0 - 0.055))
     
     ds.to_netcdf(file_out)
     print(datetime.now().isoformat())
